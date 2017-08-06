96cf398c7472cac99890cb8c61d6b22f8564a596
Javad <bc_alimohammadi@yahoo.com>
2017-08-06 23:41:29 +0430

    Not completed yet. Add entity manager module.

--- src/main/java/com/dena/platform/api/EntityDTO.java
+++ src/main/java/com/dena/platform/api/EntityDTO.java
@@ -1,14 +1,17 @@
 package com.dena.platform.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * @author Javad Alimohammadi<bs.alimohammadi@yahoo.com>
  */
 
 public class EntityDTO {
 
+    @JsonProperty("entity_id")
     private String entityId;
 
     private String entityType;
 
-    
+
 }
