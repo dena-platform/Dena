2dfca04cd96c462fd4698d09d0a0dd68c517686f
Javad <bc_alimohammadi@yahoo.com>
2017-08-06 20:31:53 +0430

    Not completed yet. Add entity manager module.

--- src/main/java/com/dena/platform/core/entity/StandardEntity.java
+++ src/main/java/com/dena/platform/core/entity/StandardEntity.java
@@ -3,9 +3,12 @@ package com.dena.platform.core.entity;
 /**
  * The most widely used base class for persistent entities. <br>
  * Optimistically locked, implements Updatable and SoftDelete.
+ * <p>
+ * <p> Borrowed from cuba platform project which is licensed under the Apache License, Version 2.0.
+ * See http://www.apache.org/licenses/LICENSE-2.0.
  *
  * @author Javad Alimohammadi<bs.alimohammadi@yahoo.com>
  */
 public class StandardEntity {
-    
+
 }
