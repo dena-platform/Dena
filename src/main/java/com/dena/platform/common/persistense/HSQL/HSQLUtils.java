56ce8b621af0ea9dbdb6f2add7028c08ba7f7e76
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2017-08-23 09:51:00 +0430

    first commit!

--- src/main/java/com/dena/platform/common/persistense/HSQL/HSQLUtils.java
+++ src/main/java/com/dena/platform/common/persistense/HSQL/HSQLUtils.java
@@ -1,17 +1,28 @@
 package com.dena.platform.common.persistense.HSQL;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  * @author Javad Alimohammadi<bs.alimohammadi@yahoo.com>
  */
 
 public class HSQLUtils {
+    private final String HSQL_CONNECTION_URL = "jdbc:hsqldb:mem:DENA_PLATFORM";
+
     public static void craeteTableIfNotExist() {
 
     }
 
     private Connection makeConnection() {
-            
+        String userName = "sa";
+        String password = "";
+
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            Connection connection = DriverManager.getConnection(HSQL_CONNECTION_URL, userName, password);
+        } catch (Exception ex) {
+
+        }
     }
 }
