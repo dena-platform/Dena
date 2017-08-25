dfd5dc0f4bb2789f41bb1170a0ee390bd4802067
Javad <bc_alimohammadi@yahoo.com>
2017-08-25 15:29:11 +0430

    not completed yet

--- src/main/java/com/dena/platform/common/persistense/HSQL/HSQLUtils.java
+++ src/main/java/com/dena/platform/common/persistense/HSQL/HSQLUtils.java
@@ -25,7 +25,6 @@ public class HSQLUtils {
                     }
                 }
         );
-
         connection
                 .ifPresent(connectionConsumer
                         .andThen(LambdaWrapper.uncheckedConsumer(HSQLUtils::closeConnection)));
