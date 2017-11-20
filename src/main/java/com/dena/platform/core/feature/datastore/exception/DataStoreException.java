0bd628569ec97fac816253710700c53b12fe28dc
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2017-11-20 18:52:51 +0330

    first commit!

---
+++ src/main/java/com/dena/platform/core/feature/datastore/exception/DataStoreException.java
@@ -0,0 +1,9 @@
+package com.dena.platform.core.feature.datastore.exception;
+
+/**
+ * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
+ */
+
+public class DataStoreException extends RuntimeException {
+
+}
