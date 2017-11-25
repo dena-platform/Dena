e2ac1990f27b715f8214a77f211663683a05ec03
Javad <bc_alimohammadi@yahoo.com>
2017-11-25 22:37:47 +0330

    change author header

---
+++ src/main/java/com/dena/platform/restapi/DenaResponse.java
@@ -0,0 +1,13 @@
+package com.dena.platform.restapi;
+
+import java.util.Date;
+
+/**
+ * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
+ */
+public class DenaResponse {
+    private Date timestamp;
+    private Integer count;
+
+    private List<>
+}
