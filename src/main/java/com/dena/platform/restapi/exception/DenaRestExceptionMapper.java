69518709f62ad90929820346cb2bdfc3587dd720
Javad <bc_alimohammadi@yahoo.com>
2017-11-29 23:53:22 +0330

    change author header

--- src/main/java/com/dena/platform/restapi/exception/DenaRestExceptionMapper.java
+++ src/main/java/com/dena/platform/restapi/exception/DenaRestExceptionMapper.java
@@ -4,6 +4,7 @@ import com.dena.platform.restapi.dto.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -57,6 +58,27 @@ public class DenaRestExceptionMapper {
         final Locale locale = Locale.getDefault();
 
         String message = messageSource.getMessage(ErrorCodes.GENERAL.getMessageCode(), null, locale);
+
+        if (ex.getCause() != null) {
+            log.error("An error occurred invoking a REST service.", ex.getCause());
+        }
+
+        ErrorResponse errorResponse = ErrorResponse.ErrorResponseBuilder.anErrorResponse()
+                .withStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+                .withErrorCode(ErrorCodes.GENERAL.getErrorCode())
+                .withMessages(message)
+                .build();
+
+        return errorResponse;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ErrorResponse handleDenaRestException(HttpServletRequest request, HttpServletResponse response, HttpMediaTypeNotSupportedException ex) {
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        final Locale locale = Locale.getDefault();
+
+        String message = messageSource.getMessage(ErrorCodes.INVALID_MEDIA_TYPE.getMessageCode(), null, locale);
 
         if (ex.getCause() != null) {
             log.error("An error occurred invoking a REST service.", ex.getCause());
