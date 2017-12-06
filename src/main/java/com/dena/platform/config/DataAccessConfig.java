79dff0a057cecc43d32934433da07920fc48fc2e
Javad <bc_alimohammadi@yahoo.com>
2017-12-06 13:01:02 +0330

    change author header

--- src/main/java/com/dena/platform/config/DataAccessConfig.java
+++ src/main/java/com/dena/platform/config/DataAccessConfig.java
@@ -1,13 +1,6 @@
 package com.dena.platform.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
 
 
 /**
@@ -16,22 +9,16 @@ import javax.sql.DataSource;
 @Configuration
 public class DataAccessConfig {
 
-    @Profile("dev")
-    @Bean
-    public DataSource dataSourceDev() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).build();
-        return db;
-    }
-
-    // TODO: 2017-07-17 change data source for production config
-    @Profile("production")
-    @Bean
-    public DataSource dataSourceProduction() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).build();
-        return db;
-    }
+//    @Profile("dev")
+//    @Bean
+//    public DataSource dataSourceDev() {
+//    }
+//
+//    // TODO: 2017-07-17 change data source for production config
+//    @Profile("production")
+//    @Bean
+//    public DataSource dataSourceProduction() {
+//    }
 
 
 }
