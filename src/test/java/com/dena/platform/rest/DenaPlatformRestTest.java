4e2710a9454b6610b2111a15d98f1b507ae21f7f
Javad <bc_alimohammadi@yahoo.com>
2017-12-08 22:52:57 +0330

    change author header

---
+++ src/test/java/com/dena/platform/rest/DenaPlatformRestTest.java
@@ -0,0 +1,40 @@
+package com.dena.platform.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DenaPlatformRestTest {
+
+    private MockMvc mockMvc;
+
+    @Resource
+    private WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void contextLoads() {
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/v1/denaQA/posts/5a206dc2cc2a9b26e483d664"))
+                .andExpect(ResultMatcher)
+    }
+
+}
