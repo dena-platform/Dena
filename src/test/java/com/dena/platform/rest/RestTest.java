758ba8ffbdb6a99b8f662aaaffdcf78dc06556c9
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2017-12-12 13:13:19 +0330

    first commit!

--- src/test/java/com/dena/platform/rest/RestTest.java
+++ src/test/java/com/dena/platform/rest/RestTest.java
@@ -1,5 +1,12 @@
 package com.dena.platform.rest;
 
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +20,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DenaPlatformRestTest {
+public class RestTest {
 
     private MockMvc mockMvc;
 
@@ -24,13 +32,26 @@ public class DenaPlatformRestTest {
     private WebApplicationContext wac;
 
     @Before
-    public void setup() {
+    public void setupClass() throws IOException {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+        MongodExecutable mongodBExe = starter.prepare(new MongodConfigBuilder()
+                .net(new Net("localhost", 12345, Network.localhostIsIPv6()))
+                .build());
+
+        MongodProcess mongodProcess = mongodBExe.start();
+
     }
 
     @Test
-    public void testFindObjectsTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/denaQA/posts/5a206dc2cc2a9b26e483d664"))
+    public void testCreateSingleObject() {
+
+    }
+
+    @Test
+    public void testFindObjects() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/denaBlogger/posts/5a206dc2cc2a9b26e483d664"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
