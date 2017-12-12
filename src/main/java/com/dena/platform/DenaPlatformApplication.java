169ddb51940669c4f362ec7dd0b8150ec09a29d2
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2017-12-12 18:02:48 +0330

    first commit!

--- src/main/java/com/dena/platform/DenaPlatformApplication.java
+++ src/main/java/com/dena/platform/DenaPlatformApplication.java
@@ -2,9 +2,9 @@ package com.dena.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {EmbeddedMongoAutoConfiguration.class})
 public class DenaPlatformApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/dena/platform/rest/RestTest.java
+++ src/test/java/com/dena/platform/rest/RestTest.java
@@ -4,12 +4,13 @@ import de.flapdoodle.embed.mongo.MongodExecutable;
 import de.flapdoodle.embed.mongo.MongodProcess;
 import de.flapdoodle.embed.mongo.MongodStarter;
 import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,23 +27,31 @@ import java.io.IOException;
 @SpringBootTest
 public class RestTest {
 
+    private final static Logger log = LoggerFactory.getLogger(RestTest.class);
+
     private MockMvc mockMvc;
+
+    private MongodProcess mongodProcess;
 
     @Resource
     private WebApplicationContext wac;
 
     @Before
-    public void setupClass() throws IOException {
+    public void setup() throws IOException {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
 
         MongodStarter starter = MongodStarter.getDefaultInstance();
         MongodExecutable mongodBExe = starter.prepare(new MongodConfigBuilder()
                 .version(Version.Main.V3_2)
-                .net(new Net("localhost", 12345, Network.localhostIsIPv6()))
                 .build());
 
-        MongodProcess mongodProcess = mongodBExe.start();
+        this.mongodProcess = mongodBExe.start();
 
+    }
+
+    @After
+    public void tearDown() {
+        mongodProcess.stop();
     }
 
 //    @Test
