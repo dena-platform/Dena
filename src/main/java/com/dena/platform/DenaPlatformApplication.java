c219ddab61ab44d87400cbf68cf9131610c3eeb6
Javad <bc_alimohammadi@yahoo.com>
2017-12-15 13:46:15 +0330

    change author header

--- src/main/java/com/dena/platform/DenaPlatformApplication.java
+++ src/main/java/com/dena/platform/DenaPlatformApplication.java
@@ -3,9 +3,8 @@ package com.dena.platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class DenaPlatformApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DenaPlatformApplication.class, args);
     }
