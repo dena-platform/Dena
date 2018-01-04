cbe0c8e526a493e57e1ae77153ecc967ae3af7a9
Javad <bc_alimohammadi@yahoo.com>
2018-01-05 00:16:52 +0330

    change author header

---
+++ src/test/java/com/dena/platform/rest/dto/TestErrorResponse.java
@@ -0,0 +1,24 @@
+package com.dena.platform.rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
+ */
+@JsonAutoDetect
+public class TestErrorResponse {
+    @JsonProperty("status")
+    public int status;
+
+    @JsonProperty("error_code")
+    public String errorCode;
+
+    @JsonProperty("messages")
+    public List<String> messages;
+
+
+}
