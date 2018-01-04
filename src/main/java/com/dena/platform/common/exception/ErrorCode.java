49f0e365c060da33ccd6f4bff601d5db6ca78820
Javad <bc_alimohammadi@yahoo.com>
2018-01-05 01:33:19 +0330

    change author header

--- src/main/java/com/dena/platform/common/exception/ErrorCode.java
+++ src/main/java/com/dena/platform/common/exception/ErrorCode.java
@@ -1,18 +1,22 @@
 package com.dena.platform.common.exception;
 
 /**
+ * Each error code contain two part:
+ * 1:message code that resolve to an message
+ * 2:
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
+
 public enum ErrorCode {
     // Input type & format exception - range 00-99
-    INVALID_REQUEST("com.dena.platform.restapi.exception.INVALID_REQUEST", "00"),
-    INVALID_MEDIA_TYPE("com.dena.platform.restapi.exception.INVALID_MEDIA_TYPE", "01"),
+    INVALID_REQUEST("com.dena.platform.restapi.exception.INVALID_REQUEST", "00400"),
+    INVALID_MEDIA_TYPE("com.dena.platform.restapi.exception.INVALID_MEDIA_TYPE", "01400"),
 
 
     // Data store exception - range 100-199
-    GENERAL_DATA_STORE_EXCEPTION("com.dena.platform.restapi.exception.GENERAL_DATASTORE_EXCEPTION", "100"),
-    RELATION_INVALID_EXCEPTION("com.dena.platform.restapi.exception.RELATION_INVALID_EXCEPTION", "101"),
-    ObjectId_INVALID_EXCEPTION("com.dena.platform.restapi.exception.ObjectId_INVALID_EXCEPTION", "102"),
+    GENERAL_DATA_STORE_EXCEPTION("com.dena.platform.restapi.exception.GENERAL_DATASTORE_EXCEPTION", "100500"),
+    RELATION_INVALID_EXCEPTION("com.dena.platform.restapi.exception.RELATION_INVALID_EXCEPTION", "101400"),
+    ObjectId_INVALID_EXCEPTION("com.dena.platform.restapi.exception.ObjectId_INVALID_EXCEPTION", "102400"),
 
 
     GENERAL("com.dena.platform.restapi.exception.GENERAL", "0");
