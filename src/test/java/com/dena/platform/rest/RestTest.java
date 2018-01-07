3a8c6bbad890cef792f410f954d790e4c2c22820
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2018-01-07 09:27:51 +0330

    first commit!

--- src/test/java/com/dena/platform/rest/RestTest.java
+++ src/test/java/com/dena/platform/rest/RestTest.java
@@ -1,6 +1,9 @@
 package com.dena.platform.rest;
 
-import com.dena.platform.rest.dto.*;
+import com.dena.platform.rest.dto.ReturnedObject;
+import com.dena.platform.rest.dto.TestObjectResponse;
+import com.dena.platform.rest.dto.TestRelatedObject;
+import com.dena.platform.rest.dto.TestRequestObject;
 import com.dena.platform.utils.CommonConfig;
 import com.mongodb.MongoClient;
 import org.bson.Document;
@@ -9,7 +12,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -97,103 +99,15 @@ public class RestTest {
     }
 
     @Test
-    public void test_FindObject_When_Object_Exist() throws Exception {
-
-        /////////////////////////////////////////////
-        //            Send Find Object Request
-        /////////////////////////////////////////////
-        ReturnedObject actualReturnObject = performFindRequest(objectId3);
-
-        ReturnedObject expectedReturnObject = new ReturnedObject();
-        expectedReturnObject.setCount(1L);
-        expectedReturnObject.setTimestamp(actualReturnObject.getTimestamp());
-
-        /////////////////////////////////////////////
-        //            Assert Found Object
-        /////////////////////////////////////////////
-        TestObjectResponse testObjectResponse = new TestObjectResponse();
-        testObjectResponse.objectId = objectId3;
-        testObjectResponse.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId3;
-        testObjectResponse.addProperty("name", "javad");
-        testObjectResponse.addProperty("job", "developer");
-        testObjectResponse.testRelatedObjects = Arrays.asList(new TestRelatedObject(objectId1, CommonConfig.COLLECTION_NAME), new TestRelatedObject(objectId2, CommonConfig.COLLECTION_NAME));
-        expectedReturnObject.setTestObjectResponseList(Collections.singletonList(testObjectResponse));
-
-        // check timestamp field of returned object
-        assertTrue(isTimeEqualRegardlessOfSecond(actualReturnObject.getTimestamp(), Instant.now().toEpochMilli()));
-        JSONAssert.assertEquals(createJSONFromObject(expectedReturnObject), createJSONFromObject(actualReturnObject), true);
-
-    }
-
-    @Test
-    public void test_FindRelatedObject() throws Exception {
-        /////////////////////////////////////////////
-        //            Send Find Object Request
-        /////////////////////////////////////////////
-        ReturnedObject actualReturnObject = performFindRelationRequest(objectId3, CommonConfig.COLLECTION_NAME);
-
-        /////////////////////////////////////////////
-        //            Assert Found Object
-        /////////////////////////////////////////////
-        ReturnedObject expectedReturnObject = new ReturnedObject();
-        expectedReturnObject.setCount(2L);
-        expectedReturnObject.setTimestamp(actualReturnObject.getTimestamp());
-
-
-        TestObjectResponse testObjectResponse1 = new TestObjectResponse();
-        testObjectResponse1.objectId = objectId1;
-        testObjectResponse1.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId1;
-        testObjectResponse1.addProperty("name", "javad");
-        testObjectResponse1.addProperty("job", "developer");
-
-        TestObjectResponse testObjectResponse2 = new TestObjectResponse();
-        testObjectResponse2.objectId = objectId2;
-        testObjectResponse2.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId2;
-        testObjectResponse2.addProperty("name", "javad");
-        testObjectResponse2.addProperty("job", "developer");
-
-        expectedReturnObject.setTestObjectResponseList(Arrays.asList(testObjectResponse1, testObjectResponse2));
-
-
-        // check timestamp field of returned object
-        assertTrue(isTimeEqualRegardlessOfSecond(actualReturnObject.getTimestamp(), Instant.now().toEpochMilli()));
-        JSONAssert.assertEquals(createJSONFromObject(expectedReturnObject), createJSONFromObject(actualReturnObject), false);
-
-    }
-
-    @Test
-    public void test_FindObject_When_Object_Not_Exist() throws Exception {
-
-        /////////////////////////////////////////////
-        //            Send Find Object Request
-        /////////////////////////////////////////////
-        ReturnedObject actualReturnObject = performFindRequest(randomObjectId);
-
-        /////////////////////////////////////////////
-        //            Assert Found Object
-        /////////////////////////////////////////////
-        ReturnedObject expectedReturnObject = new ReturnedObject();
-        expectedReturnObject.setCount(0L);
-        expectedReturnObject.setTimestamp(actualReturnObject.getTimestamp());
-
-        // check timestamp field of returned object
-        assertTrue(isTimeEqualRegardlessOfSecond(actualReturnObject.getTimestamp(), Instant.now().toEpochMilli()));
-        JSONAssert.assertEquals(createJSONFromObject(expectedReturnObject), createJSONFromObject(actualReturnObject), JSONCompareMode.NON_EXTENSIBLE);
-
-    }
-
-
-
-    @Test
     public void test_UpdateObject() throws Exception {
         /////////////////////////////////////////////
         //           Send Update Object Request
         /////////////////////////////////////////////
         TestRequestObject requestObject = new TestRequestObject();
         requestObject.setObjectId(objectId3);
-        requestObject.addProperty("job", "new developer");
+        requestObject.addProperty("job", "new developer value");
         requestObject.addProperty("new field", "new value");
-        String newObjectId = ObjectId.get().toString();
+        String newObjectId = randomObjectId;
         requestObject.getRelatedObjects().add(new TestRelatedObject(newObjectId, CommonConfig.COLLECTION_NAME));
 
         ReturnedObject actualReturnObject = performUpdateObject(createJSONFromObject(requestObject));
@@ -202,8 +116,8 @@ public class RestTest {
         //            Assert Update Response
         /////////////////////////////////////////////
         TestObjectResponse testObjectResponse = new TestObjectResponse();
-        testObjectResponse.objectId = "5a206dafcc2a9b26e483d663";
-        testObjectResponse.objectURI = "/denaTestCollection/5a206dafcc2a9b26e483d663";
+        testObjectResponse.objectId = objectId3;
+        testObjectResponse.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId3;
         testObjectResponse.getAllFields().put("job", "new developer");
         testObjectResponse.getAllFields().put("new field", "new value");
         testObjectResponse.testRelatedObjects = Collections.singletonList(new TestRelatedObject(newObjectId, CommonConfig.COLLECTION_NAME));
