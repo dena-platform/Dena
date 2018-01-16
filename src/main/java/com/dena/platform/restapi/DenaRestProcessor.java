fe6a9e5ec04b154a4ea4f73026717d14b29efa34
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2018-01-16 09:24:28 +0330

    first commit!

--- src/main/java/com/dena/platform/restapi/DenaRestProcessor.java
+++ src/main/java/com/dena/platform/restapi/DenaRestProcessor.java
@@ -3,8 +3,6 @@ package com.dena.platform.restapi;
 import com.dena.platform.core.DenaRequestContext;
 import org.springframework.http.ResponseEntity;
 
-import java.net.URISyntaxException;
-
 /**
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
