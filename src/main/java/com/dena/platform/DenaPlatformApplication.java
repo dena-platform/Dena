44cecdfb1065563b436f0b27fe936a2599063a76
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2018-02-27 17:26:54 +0330

    first commit!

--- src/main/java/com/dena/platform/DenaPlatformApplication.java
+++ src/main/java/com/dena/platform/DenaPlatformApplication.java
@@ -1,11 +1,16 @@
 package com.dena.platform;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DenaPlatformApplication {
+    private final static Logger log = LoggerFactory.getLogger(DenaPlatformApplication.class);
+
     public static void main(String[] args) {
+
         SpringApplication.run(DenaPlatformApplication.class, args);
     }
 
