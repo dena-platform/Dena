e6a27b10509a122fa96d8f3d0988e83c1026f3c7
Javad <bc_alimohammadi@yahoo.com>
2018-03-02 13:05:33 +0330

    first commit !!!

--- src/main/java/com/dena/platform/common/web/filter/DenaRequestFilter.java
+++ src/main/java/com/dena/platform/common/web/filter/DenaRequestFilter.java
@@ -1,12 +1,16 @@
 package com.dena.platform.common.web.filter;
 
+import com.dena.platform.common.web.DenaRequestProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -14,14 +18,19 @@ import java.io.IOException;
  */
 @Component("denaRequestFilter")
 public class DenaRequestFilter extends AbstractDenaFilter {
+    private final static Logger log = LoggerFactory.getLogger(DenaRequestFilter.class);
 
+    @Resource
+    protected DenaRequestProcessor denaRequestProcessor;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
+            denaRequestProcessor.processRequest((HttpServletRequest) request);
             chain.doFilter(request, response);
         } catch (Exception ex) {
-            
+        } finally {
+            denaRequestProcessor.postProcess();
         }
     }
 }
