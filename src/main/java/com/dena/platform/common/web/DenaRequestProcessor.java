654bfcbee45aeefb7fb4a8c360cc6cbce96d3b3d
Javad <bc_alimohammadi@yahoo.com>
2018-03-02 13:11:47 +0330

    first commit !!!

--- src/main/java/com/dena/platform/common/web/DenaRequestProcessor.java
+++ src/main/java/com/dena/platform/common/web/DenaRequestProcessor.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServletRequest;
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
 public interface DenaRequestProcessor {
+    /**
+     * Process request after security.
+     * @param request
+     */
     void processRequest(HttpServletRequest request);
 
     void postProcess();
