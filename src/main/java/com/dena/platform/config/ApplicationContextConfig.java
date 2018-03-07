86ffa244fab53394b6dbbd997a6286fa298c7c54
Javad <bc_alimohammadi@yahoo.com>
2018-03-08 01:42:05 +0330

    Add banner

--- src/main/java/com/dena/platform/config/ApplicationContextConfig.java
+++ src/main/java/com/dena/platform/config/ApplicationContextConfig.java
@@ -1,11 +1,13 @@
 package com.dena.platform.config;
 
 import com.dena.platform.common.config.DenaConfigReader;
+import com.fasterxml.jackson.databind.MapperFeature;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
@@ -27,5 +29,14 @@ public class ApplicationContextConfig {
         messageSource.setCacheSeconds(messageCacheSeconds);
         messageSource.setUseCodeAsDefaultMessage(useCodeAsDefaultMessage);
         return messageSource;
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.featuresToEnable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+
+        return builder;
     }
 }
