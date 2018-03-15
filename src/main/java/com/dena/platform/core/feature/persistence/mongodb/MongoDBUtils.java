2ffc532c778e5edcac239605c1133152972eb255
Javad <bc_alimohammadi@yahoo.com>
2018-03-15 17:54:47 +0330

    refactor delete operation

--- src/main/java/com/dena/platform/core/feature/persistence/mongodb/MongoDBUtils.java
+++ src/main/java/com/dena/platform/core/feature/persistence/mongodb/MongoDBUtils.java
@@ -101,8 +101,8 @@ public class MongoDBUtils {
     }
 
     public static long deleteRelationWithObjectId(MongoDatabase mongoDatabase, String parentType, String parentObjectId, String relationName, String childObjectId) {
-        Bson searchDocument = Filters.eq(ID, new ObjectId(parentObjectId));
-        Bson deleteObjectIdCommand = Updates.pull(relationName + "." + RELATION_IDS, new BsonObjectId(new ObjectId(childObjectId)));
+        Bson searchDocument = findDocumentById(mongoDatabase, parentType, parentObjectId).get(0).get(relationName).asDocument();
+        Bson deleteObjectIdCommand = Updates.pull(RELATION_IDS, new BsonObjectId(new ObjectId(childObjectId)));
 
 
         UpdateResult updateResult = mongoDatabase
