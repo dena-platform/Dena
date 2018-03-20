912678fb81debd32fea090ee8d28da6fab6e87ea
Javad <bc_alimohammadi@yahoo.com>
2018-03-20 23:52:35 +0430

    Add uml document

---
+++ src/main/java/com/dena/platform/core/feature/security/domain/APP.java
@@ -0,0 +1,16 @@
+package com.dena.platform.core.feature.security.domain;
+
+/**
+ * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
+ */
+public class APP {
+    private String appName;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+}
