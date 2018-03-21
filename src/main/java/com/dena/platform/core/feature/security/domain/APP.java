abbca7479c3f85ee3b9cc7d13613e4d0834fcdac
Javad <bc_alimohammadi@yahoo.com>
2018-03-21 22:40:46 +0430

    Support new user registration for existing app

--- src/main/java/com/dena/platform/core/feature/security/domain/APP.java
+++ src/main/java/com/dena/platform/core/feature/security/domain/APP.java
@@ -4,13 +4,13 @@ package com.dena.platform.core.feature.security.domain;
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
 public class APP {
-    private String appName;
+    private String name;
 
-    public String getAppName() {
-        return appName;
+    public String getName() {
+        return name;
     }
 
-    public void setAppName(String appName) {
-        this.appName = appName;
+    public void setName(String name) {
+        this.name = name;
     }
 }
