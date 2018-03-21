bd9db5d1c2b48aff856e24cd5df998df8351ff05
Javad <bc_alimohammadi@yahoo.com>
2018-03-22 01:26:50 +0430

    Support new user registration for existing app

--- src/main/java/com/dena/platform/core/feature/security/DenaUserManagement.java
+++ src/main/java/com/dena/platform/core/feature/security/DenaUserManagement.java
@@ -12,18 +12,22 @@ public interface DenaUserManagement {
     /**
      * Register an user in app.<br>
      * If user exist then throw {@link UserManagementException} exception.
+     *
      * @param user
      * @return
      * @throws UserManagementException
      */
-    DenaObject registerUser(User user);
+    DenaObject registerUser(String appId, User user);
 
     /**
      * Check if user is registered in dena platform before
+     *
+     *
+     * @param appId
      * @param user
      * @return
      */
-    boolean isUserExist(User user);
+    boolean isUserExist(String appId, User user);
 
 
 }
