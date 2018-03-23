59c6443f3e1ba3d159338587f52cb7385e896044
Javad <bc_alimohammadi@yahoo.com>
2018-03-24 00:15:28 +0430

    Support new user registration for existing app

--- src/main/java/com/dena/platform/config/SecurityConfig.java
+++ src/main/java/com/dena/platform/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.dena.platform.config;
 
 import com.dena.platform.common.config.DenaConfigReader;
 import com.dena.platform.common.utils.BeanFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -15,7 +17,12 @@ public class SecurityConfig {
 
     @Bean("passwordEncoder")
     public PasswordEncoder passwordEncoderBean() {
-        String passwordEncoderName = DenaConfigReader.readProperty("dena.security.password.encoder", "org.springframework.security.crypto.password.NoOpPasswordEncoder");
+        String passwordEncoderName = DenaConfigReader.readProperty("dena.security.password.encoder");
+
+        if (StringUtils.isEmpty(passwordEncoderName)) {
+            return NoOpPasswordEncoder.getInstance();
+        }
+
         return (PasswordEncoder) BeanFactory.createInstance(passwordEncoderName);
     }
 
