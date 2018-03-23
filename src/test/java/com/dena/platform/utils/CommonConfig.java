0a52c751c41f19ebb1057c6bae99ed3ea5b2c920
Javad <bc_alimohammadi@yahoo.com>
2018-03-23 19:44:02 +0430

    Support new user registration for existing app

--- src/test/java/com/dena/platform/utils/CommonConfig.java
+++ src/test/java/com/dena/platform/utils/CommonConfig.java
@@ -6,7 +6,12 @@ package com.dena.platform.utils;
 
 public interface CommonConfig {
     String APP_ID = "denaTestDB";
+
     String COLLECTION_NAME = "denaTestCollection";
+
     String RELATION_NAME = "test_relation_name";
+
     String BASE_URL = "/v1/" + APP_ID + "/" + COLLECTION_NAME;
+
+    String REGISTER_URL = "/v1/" + APP_ID + "/users/register";
 }
