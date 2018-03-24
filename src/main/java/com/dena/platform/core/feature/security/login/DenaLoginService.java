4fccb47203b7579103d846094306dd16ebcbcd2a
Javad <bc_alimohammadi@yahoo.com>
2018-03-25 01:13:06 +0430

    add login feature

--- src/main/java/com/dena/platform/core/feature/security/login/DenaLoginService.java
+++ src/main/java/com/dena/platform/core/feature/security/login/DenaLoginService.java
@@ -5,7 +5,7 @@ import com.dena.platform.core.feature.user.domain.User;
 /**
  * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
-public interface LoginService {
+public interface DenaLoginService {
     User loginUser(String email, String rawPassword);
 
     boolean logoutUser(String email);
