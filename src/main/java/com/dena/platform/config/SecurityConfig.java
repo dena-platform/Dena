84312b5bbba644490d9e573d4972fda42d124b25
Javad <bc_alimohammadi@yahoo.com>
2018-03-25 01:13:06 +0430

    add login feature

--- src/main/java/com/dena/platform/config/SecurityConfig.java
+++ src/main/java/com/dena/platform/config/SecurityConfig.java
@@ -5,7 +5,9 @@ import com.dena.platform.common.utils.BeanFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,7 +17,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+
+    }
 
 
     @Bean("passwordEncoder")
