6076fcfb1de46bf0120c39f47a853a9008440bca
nazarpour <abdollah.nazarpour@yahoo.com>
2018-03-30 19:07:58 +0430

    authentication works properly, still some todo needs to be done;

--- src/test/java/com/dena/platform/rest/user/UserManagement.java
+++ src/test/java/com/dena/platform/rest/user/UserManagement.java
@@ -158,7 +158,7 @@ public class UserManagement extends AbstractDataStoreTest {
     /////////////////////////////////////////////
     //            USER MANAGEMENT REQUEST
     /////////////////////////////////////////////
-    protected <T> T performRegisterUser(String body, HttpStatus httpStatus, Class<T> klass) throws Exception {
+    public <T> T performRegisterUser(String body, HttpStatus httpStatus, Class<T> klass) throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(CommonConfig.REGISTER_USER_URL)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(body))
