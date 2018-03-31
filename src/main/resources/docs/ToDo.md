b0fe4e4b57855d7db0e6fdc0141a1493a4dcae97
Javad <bc_alimohammadi@yahoo.com>
2018-03-31 11:30:46 +0430

    add paging feature

--- src/main/resources/docs/ToDo.md
+++ src/main/resources/docs/ToDo.md
@@ -3,6 +3,6 @@
 This is a list of TO-DO / feature we should implement in first release
 
 - User defined type can not start with "DENA_ ". Type that start with "DENA_ "  are system defined and can not use by developer. 
-- Create an API for findAll Service.
+
 
 
--- src/test/java/com/dena/platform/rest/persistence/FindDataTest.java
+++ src/test/java/com/dena/platform/rest/persistence/FindDataTest.java
@@ -4,7 +4,6 @@ import com.dena.platform.rest.dto.TestDenaResponseDTO;
 import com.dena.platform.rest.dto.TestObjectResponseDTO;
 import com.dena.platform.utils.CommonConfig;
 import org.junit.Test;
-import org.junit.runners.Parameterized;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
@@ -54,21 +53,29 @@ public class FindDataTest extends AbstractDataStoreTest {
         /////////////////////////////////////////////
         //            Send Find Object Request
         /////////////////////////////////////////////
-        TestDenaResponseDTO actualReturnObject = performFindRequestInTable(CommonConfig.COLLECTION_NAME, 0, 2);
+        TestDenaResponseDTO actualReturnObject1 = performFindRequestInTable(CommonConfig.COLLECTION_NAME, 0, 2);
+        TestDenaResponseDTO actualReturnObject2 = performFindRequestInTable(CommonConfig.COLLECTION_NAME, 4, 5);
 
-        TestDenaResponseDTO expectedReturnObject = new TestDenaResponseDTO();
-        expectedReturnObject.foundObjectCount = 2L;
-        expectedReturnObject.timestamp = actualReturnObject.timestamp;
+        TestDenaResponseDTO expectedReturnObject1 = new TestDenaResponseDTO();
+        expectedReturnObject1.foundObjectCount = 2L;
+        expectedReturnObject1.timestamp = actualReturnObject1.timestamp;
+
+        TestDenaResponseDTO expectedReturnObject2 = new TestDenaResponseDTO();
+        expectedReturnObject2.foundObjectCount = 5L;
+        expectedReturnObject2.timestamp = actualReturnObject2.timestamp;
+
+
 
         /////////////////////////////////////////////
         //            Assert Found Object
         /////////////////////////////////////////////
+
+        // Expected result for request 1
         TestObjectResponseDTO testObjectResponseDTO1 = new TestObjectResponseDTO();
         testObjectResponseDTO1.objectId = objectId1;
         testObjectResponseDTO1.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId1;
         testObjectResponseDTO1.addProperty("name", "javad");
         testObjectResponseDTO1.addProperty("job", "developer");
-        expectedReturnObject.setTestObjectResponseDTOList(Collections.singletonList(testObjectResponseDTO1));
 
         TestObjectResponseDTO testObjectResponseDTO2 = new TestObjectResponseDTO();
         testObjectResponseDTO2.objectId = objectId2;
@@ -76,12 +83,50 @@ public class FindDataTest extends AbstractDataStoreTest {
         testObjectResponseDTO2.addProperty("name", "javad");
         testObjectResponseDTO2.addProperty("job", "developer");
 
-        expectedReturnObject.setTestObjectResponseDTOList(Arrays.asList(testObjectResponseDTO1, testObjectResponseDTO2));
+        expectedReturnObject1.setTestObjectResponseDTOList(Arrays.asList(testObjectResponseDTO1, testObjectResponseDTO2));
+
+        // Expected result for request 2
+        TestObjectResponseDTO testObjectResponseDTO3 = new TestObjectResponseDTO();
+        testObjectResponseDTO3.objectId = objectId5;
+        testObjectResponseDTO3.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId5;
+        testObjectResponseDTO3.addProperty("name", "javad");
+        testObjectResponseDTO3.addProperty("job", "developer");
+
+        TestObjectResponseDTO testObjectResponseDTO4 = new TestObjectResponseDTO();
+        testObjectResponseDTO4.objectId = objectId6;
+        testObjectResponseDTO4.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId6;
+        testObjectResponseDTO4.addProperty("name", "javad");
+        testObjectResponseDTO4.addProperty("job", "developer");
+
+        TestObjectResponseDTO testObjectResponseDTO5 = new TestObjectResponseDTO();
+        testObjectResponseDTO5.objectId = objectId7;
+        testObjectResponseDTO5.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId7;
+        testObjectResponseDTO5.addProperty("name", "javad");
+        testObjectResponseDTO5.addProperty("job", "developer");
+
+        TestObjectResponseDTO testObjectResponseDTO6 = new TestObjectResponseDTO();
+        testObjectResponseDTO6.objectId = objectId8;
+        testObjectResponseDTO6.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId8;
+        testObjectResponseDTO6.addProperty("name", "javad");
+        testObjectResponseDTO6.addProperty("job", "developer");
+
+        TestObjectResponseDTO testObjectResponseDTO7 = new TestObjectResponseDTO();
+        testObjectResponseDTO7.objectId = objectId9;
+        testObjectResponseDTO7.objectURI = "/" + CommonConfig.COLLECTION_NAME + "/" + objectId9;
+        testObjectResponseDTO7.addProperty("name", "javad");
+        testObjectResponseDTO7.addProperty("job", "developer");
+
+        expectedReturnObject2.setTestObjectResponseDTOList(Arrays.asList(
+                testObjectResponseDTO3, testObjectResponseDTO4, testObjectResponseDTO5,
+                testObjectResponseDTO6, testObjectResponseDTO7
+        ));
 
 
         // check timestamp field of returned object
-        assertTrue(isTimeEqualRegardlessOfSecond(actualReturnObject.timestamp, Instant.now().toEpochMilli()));
-        JSONAssert.assertEquals(createJSONFromObject(expectedReturnObject), createJSONFromObject(actualReturnObject), JSONCompareMode.NON_EXTENSIBLE);
+        assertTrue(isTimeEqualRegardlessOfSecond(actualReturnObject1.timestamp, Instant.now().toEpochMilli()));
+        assertTrue(isTimeEqualRegardlessOfSecond(actualReturnObject2.timestamp, Instant.now().toEpochMilli()));
+        JSONAssert.assertEquals(createJSONFromObject(expectedReturnObject1), createJSONFromObject(actualReturnObject1), JSONCompareMode.NON_EXTENSIBLE);
+        JSONAssert.assertEquals(createJSONFromObject(expectedReturnObject2), createJSONFromObject(actualReturnObject2), JSONCompareMode.NON_EXTENSIBLE);
 
     }
 
