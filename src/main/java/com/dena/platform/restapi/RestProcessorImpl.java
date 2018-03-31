bc37e3413deb83278ebd9eeb9e0344581a747a6d
nazarpour <abdollah.nazarpour@yahoo.com>
2018-03-31 17:23:35 +0430

    authentication finished

--- src/main/java/com/dena/platform/restapi/RestProcessorImpl.java
+++ src/main/java/com/dena/platform/restapi/RestProcessorImpl.java
@@ -13,7 +13,7 @@ import com.dena.platform.core.feature.app.service.DenaApplicationManagement;
 import com.dena.platform.core.feature.persistence.DenaDataStore;
 import com.dena.platform.core.feature.persistence.DenaPager;
 import com.dena.platform.core.feature.persistence.exception.DataStoreException;
-import com.dena.platform.core.feature.security.JsonWebTokenGenerator;
+import com.dena.platform.core.feature.security.TokenService;
 import com.dena.platform.core.feature.user.domain.User;
 import com.dena.platform.core.feature.user.service.DenaUserManagement;
 import com.dena.platform.restapi.dto.response.DenaObjectResponse;
@@ -352,7 +352,7 @@ public class RestProcessorImpl implements DenaRestProcessor {
     }
 
     @Resource
-    private JsonWebTokenGenerator jwtGenerator;
+    private TokenService tokenService;
 
     //// TODO: remove form hear
     @Override
@@ -363,11 +363,10 @@ public class RestProcessorImpl implements DenaRestProcessor {
         String requestBody = denaRequestContext.getRequestBody();
         User user = JSONMapper.createObjectFromJSON(requestBody, User.class);
 
-        String token = jwtGenerator.generate(appId, user);
+        String token = tokenService.generate(appId, user);
 
         TokenGenResponse response = new TokenGenResponse();
         response.setToken(token);
         return ResponseEntity.ok().body(response);
     }
-
 }
