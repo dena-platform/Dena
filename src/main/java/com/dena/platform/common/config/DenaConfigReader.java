83e22aa5cd316debde27332c95ab03ee7b35da41
javad <bc_alimohammadi@yahoo.com>
2018-04-18 23:20:56 +0430

    change email setting

--- src/main/java/com/dena/platform/common/config/DenaConfigReader.java
+++ src/main/java/com/dena/platform/common/config/DenaConfigReader.java
@@ -1,25 +1,18 @@
 package com.dena.platform.common.config;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 /**
- * @author Javad Alimohammadi [<bs.alimohammadi@gmail.com>]
+ * @author Javad Alimohammadi [<bs.alimohammadi@yahoo.com>]
  */
-@Service("denaConfigReader")
-@Lazy(value = false)
-public class DenaConfigReader implements ApplicationContextAware {
+@Component("denaConfigReader")
+public class DenaConfigReader {
 
 
     private static Environment env;
-
-    private static ApplicationContext applicationContext;
 
     @Resource
     public void setEnv(Environment env) {
@@ -31,7 +24,7 @@ public class DenaConfigReader implements ApplicationContextAware {
     }
 
     public static String readProperty(final String povertyName, final String defaultValue) {
-        return getDenaConfigReader().env.getProperty(povertyName, defaultValue);
+        return env.getProperty(povertyName, defaultValue);
     }
 
     public static boolean readBooleanProperty(final String povertyName) {
@@ -39,7 +32,7 @@ public class DenaConfigReader implements ApplicationContextAware {
     }
 
     public static boolean readBooleanProperty(final String povertyName, final boolean defaultValue) {
-        return getDenaConfigReader().env.getProperty(povertyName, Boolean.class, defaultValue);
+        return env.getProperty(povertyName, Boolean.class, defaultValue);
     }
 
     public static long readLongProperty(final String povertyName) {
@@ -47,7 +40,7 @@ public class DenaConfigReader implements ApplicationContextAware {
     }
 
     public static long readLongProperty(final String povertyName, long defaultValue) {
-        return getDenaConfigReader().env.getProperty(povertyName, Long.class, defaultValue);
+        return env.getProperty(povertyName, Long.class, defaultValue);
     }
 
     public static int readIntProperty(final String povertyName) {
@@ -55,16 +48,8 @@ public class DenaConfigReader implements ApplicationContextAware {
     }
 
     public static int readIntProperty(final String povertyName, int defaultValue) {
-        return getDenaConfigReader().env.getProperty(povertyName, Integer.class, defaultValue);
-    }
-
-    private static DenaConfigReader getDenaConfigReader() {
-        return applicationContext.getBean(DenaConfigReader.class);
+        return env.getProperty(povertyName, Integer.class, defaultValue);
     }
 
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        DenaConfigReader.applicationContext = applicationContext;
-    }
 }
