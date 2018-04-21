a26efd939334b729942805a72f785fd7aba30176
Javad Alimohammadi <bc_alimohammadi@yahoo.com>
2018-04-21 16:16:24 +0430

    add cross-origin support

--- src/main/java/com/dena/platform/restapi/login/LoginController.java
+++ src/main/java/com/dena/platform/restapi/login/LoginController.java
@@ -3,6 +3,7 @@ package com.dena.platform.restapi.login;
 import com.dena.platform.restapi.DenaRestProcessor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class LoginController {
     @Resource(name = "denaRestEntityProcessorImpl")
     protected DenaRestProcessor denaRestProcessor;
 
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(path = {"/login/{app-id}"}, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity login() {
         return denaRestProcessor.login();
